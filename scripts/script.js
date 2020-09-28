9523a299a7353b4a66d0021a258b676c24f22dc0
Sarley Durango <sdurango04@misena.edu.co>
2020-09-28 16:59:57 -0500

    Solucion de comflijos en el boton dark mode

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,8 @@ jQuery('document').ready(function($){
     });
 
 });
+const btnSwitch = document.querySelector('#switch');
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+})
