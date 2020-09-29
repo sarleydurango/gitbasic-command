dd15f6794284962abe277454e8de6becb25a8064
Sarley Durango <sdurango04@misena.edu.co>
2020-09-29 15:03:27 -0500

    Actualización modo nocturno con localstorage de javascript

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,26 @@ jQuery('document').ready(function($){
     });
 
 });
+//Boton dark mode.
 const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
+
+    //Guardamos el modo en localstorage.
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else{
+        localStorage.setItem('dark-mode', 'false');
+    }
 })
+
+//Obtenemos el modo actual.
+if(localStorage.getItem('dark-mode') == 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
